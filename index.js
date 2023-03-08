7cb686cd93ec3b13deef88016dd49423f47ac41d
Alexis Romero Mendoza <alexis.rm162917@gmail.com>
2023-03-08 12:30:37 -0700

    primera version, cosumiendo la API de pokemon, usando fetch, agregando estilos con CSS puro

---
+++ index.js
@@ -0,0 +1,30 @@
+const URL = 'https://pokeapi.co/api/v2/pokemon/';
+const searchInput = document.getElementById('search');
+const pokedexContainer = document.getElementById('pokemon-info');
+
+const searchPokemon = async () => {
+    const searchedPokemon = searchInput.value.toLowerCase();
+    try {
+        const response = await fetch(URL + searchedPokemon);
+        if (!response.ok) {
+            alert('The pokemon does not exist!!');
+            return;
+        }
+        const data = await response.json();
+
+        pokedexContainer.innerHTML =
+            `
+            <img src='${data.sprites.front_default}' id='photo-pokemon'>
+            <div id='data-container'>
+            <h2>${data.name}</h2>
+                <p>number: ${data.id}</p>
+                <p>height: ${data.height / 10}m</p>
+                <p>weight: ${data.weight / 10}kg</p>
+            </div>
+            `;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.getElementById('btn-search').addEventListener('click', searchPokemon);
